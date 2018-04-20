6f0eae535e68c33870b5461e06453f33d08fde36
sdaniella <sar.daniella97@gmail.com>
2018-04-20 14:07:05 +0200

    modositott

--- src/Killerproto/SlipperyField.java
+++ src/Killerproto/SlipperyField.java
@@ -5,6 +5,20 @@ public class SlipperyField extends Field
 
     public void Accept(Crate c, Direction d) {
     	c.ChangeWeight(this);
+    	if(getCurrentThing()!=null) {
+    		getCurrentThing().PushedBy(c, neighbors.get(d), d);
+    	}
+    	
+    	if(getCurrentThing()==null) {
+    		setCurrentThing(c);
+    		Field f= neighbors.get(negDirection(d));
+    		if(f!=null) {
+    			f.Remove(c);
+    		}
+    		if(targetCrate==c) {
+        		//A helyen van a lada juhuuu
+        	}
+    	}
     }
 
 }
